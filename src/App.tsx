d9b2b807ccf43af926ebda1829d6f621c68562c4
Ryan Gilmartin <ryan22gil@gmail.com>
2022-10-20 12:05:49 -0400

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
+    const [value, setValue] = useState<number>(0);
+
     return (
         <div className="App">
             <h1>
@@ -11,6 +14,10 @@ function App(): JSX.Element {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
             </header>
+            <Button OnClick={() => setValue(value+1)}>+</Button>
+            <Button OnClick={() => setValue(value-1)}>-</Button>
+            <Button OnClick={() => setValue(0)}>reset</Button>
+            <h1>Count: {value}</h1>
         </div>
     );
 }
